ff0ea205d2849178f1e5bde6212b4b4b4fe09da3
Stuart Stein <qmstuart@gmail.com>
2025-12-06 00:14:16 +0000

    Stuff

--- AOC2025CSharp/Day2.cs
+++ AOC2025CSharp/Day2.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace AOC2025CSharp;
 
--- AOC2025CSharp/Program.cs
+++ AOC2025CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using AOC2025CSharp;
 
 Stopwatch sw = new Stopwatch();
@@ -20,5 +21,6 @@ Day5.Part1();
 Day5.Part2();
 
 sw.Stop();
+
 Console.WriteLine($"");
 Console.WriteLine($"Total Elapsed - {sw.Elapsed}");
